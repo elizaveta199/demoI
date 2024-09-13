7d21bffaf1486ab7d25dfb44733b67b690fb5838
Elizaveta I <ishutinaem@gmail.com>
2024-09-13 22:21:29 +0300

    initial commit

---
+++ src/main/java/ru/stepup/demo/MailApp.java
@@ -0,0 +1,18 @@
+package ru.stepup.demo;
+
+public class MailApp {
+    public static void main(String[] args) {
+        System.out.println(1);
+        //TODO CAT
+        doSomething();
+        System.out.println(2);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("A" + i);
+        }
+
+    }
+
+    private static void doSomething() {
+    }
+
+}
