e0db5f46ec4a747665f4f82a5a10096a166bdd01
Elizaveta I <ishutinaem@gmail.com>
2024-09-13 23:23:03 +0300

    task done

--- src/main/java/ru/stepup/demo/MailApp.java
+++ src/main/java/ru/stepup/demo/MailApp.java
@@ -7,7 +7,7 @@ public class MailApp {
         doSomething();
         System.out.println(2);
         for (int i = 0; i < 3; i++) {
-            System.out.println(8);
+            System.out.println("Hello World");
             System.out.println(7);
             System.out.println("liza");
         }
